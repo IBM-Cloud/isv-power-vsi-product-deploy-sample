1c24193728d455b4b3fb6f79007b0e8ca853eb55
Paul Finley <pbfinley1911@gmail.com>
2022-08-08 08:57:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ locals {
   catalog_image = [for x in data.ibm_pi_catalog_images.catalog_images.images : x if x.name == local.stock_image_name]
   private_image = [for x in data.ibm_pi_images.cloud_instance_images.image_info : x if x.name == local.stock_image_name]
   private_image_id = length(local.private_image) > 0 ? local.private_image[0].id  : ""
-  placement_group = [for x in data.ibm_pi_placement_groups.cloud_instance_groups.placement_group : x if x.name == var.placement_group]
+  placement_group = [for x in data.ibm_pi_placement_groups.cloud_instance_groups.placement_groups : x if x.name == var.placement_group]
   placement_group_id = length(local.placement_group) > 0 ? local.placement_group[0].id : ""
 }
 
