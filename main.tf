34291648316fb7a119d346826dce04c33bb261be
Paul Finley <pbfinley1911@gmail.com>
2022-08-15 17:05:48 -0500

    Add dynamic block for optional args

--- main.tf
+++ main.tf
@@ -3,13 +3,27 @@ data "ibm_pi_catalog_images" "catalog_images" {
   vtl                  = true
   pi_cloud_instance_id = local.pid
 }
-
 data "ibm_pi_images" "cloud_instance_images" {
   pi_cloud_instance_id = local.pid
 }
-
 data "ibm_pi_placement_groups" "cloud_instance_groups" {
   pi_cloud_instance_id = local.pid
+}
+data "ibm_pi_key" "key" {
+  pi_cloud_instance_id = local.pid
+  pi_key_name          = var.ssh_key_name
+}
+data "ibm_pi_network" "network_1" {
+  pi_cloud_instance_id = local.pid
+  pi_network_name      = var.network_1
+}
+data "ibm_pi_network" "network_2" {
+  pi_cloud_instance_id = local.pid
+  pi_network_name      = var.network_2
+}
+data "ibm_pi_network" "network_3" {
+  pi_cloud_instance_id = local.pid
+  pi_network_name      = var.network_3
 }
 
 locals {
@@ -19,26 +33,11 @@ locals {
   private_image_id = length(local.private_image) > 0 ? local.private_image[0].id  : ""
   placement_group = [for x in data.ibm_pi_placement_groups.cloud_instance_groups.placement_groups : x if x.name == var.placement_group]
   placement_group_id = length(local.placement_group) > 0 ? local.placement_group[0].id : ""
-}
-
-data "ibm_pi_key" "key" {
-  pi_cloud_instance_id = local.pid
-  pi_key_name          = var.ssh_key_name
-}
-
-data "ibm_pi_network" "network_1" {
-  pi_cloud_instance_id = local.pid
-  pi_network_name      = var.network_1
-}
-
-data "ibm_pi_network" "network_2" {
-  pi_cloud_instance_id = local.pid
-  pi_network_name      = var.network_2
-}
-
-data "ibm_pi_network" "network_3" {
-  pi_cloud_instance_id = local.pid
-  pi_network_name      = var.network_3
+  options {
+    pi_key_pair_name = length(data.ibm_pi_key.key) > 0 ? data.ibm_pi_key.key.id : ""
+    pi_affinity_policy = length(var.affinity_policy) > 0 ? var.affinity_policy : ""
+    pi_affinity_instances = length(var.pvm_instances) > 0 ? [var.pvm_instances] : ""
+  }
 }
 
 resource "ibm_pi_image" "stock_image_copy" {
@@ -57,9 +56,12 @@ resource "ibm_pi_instance" "instance" {
   pi_instance_name     = var.instance_name
   pi_proc_type         = var.processor_type
   pi_image_id          = length(local.private_image_id) == 0 ? ibm_pi_image.stock_image_copy[0].image_id : local.private_image_id
-  pi_key_pair_name     = data.ibm_pi_key.key.id
   pi_sys_type          = var.sys_type
   pi_storage_type      = var.storage_type
+  pi_license_repository_capacity = var.license_repository_capacity
+  for_each    = {for k,v in local.options: k => v if v != null}
+  key         = each.key
+  value       = each.value
   pi_network {
     network_id = data.ibm_pi_network.network_1.id
   }
@@ -69,5 +71,4 @@ resource "ibm_pi_instance" "instance" {
   pi_network {
     network_id = data.ibm_pi_network.network_3.id
   }
-  pi_license_repository_capacity = var.license_repository_capacity
 }
