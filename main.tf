74a9a191a04fbc68595f087b501e225f9ea4d633
Paul Finley <pbfinley1911@gmail.com>
2022-08-15 19:22:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,11 +33,6 @@ locals {
   private_image_id = length(local.private_image) > 0 ? local.private_image[0].id  : ""
   placement_group = [for x in data.ibm_pi_placement_groups.cloud_instance_groups.placement_groups : x if x.name == var.placement_group]
   placement_group_id = length(local.placement_group) > 0 ? local.placement_group[0].id : ""
-  options = {
-    pi_key_pair_name = length(data.ibm_pi_key.key) > 0 ? data.ibm_pi_key.key.id : null
-    pi_affinity_policy = length(var.affinity_policy) > 0 ? var.affinity_policy : null
-    pi_affinity_instances = length(var.pvm_instances) > 0 ? [var.pvm_instances] : null
-  }
 }
 
 resource "ibm_pi_image" "stock_image_copy" {
@@ -50,7 +45,6 @@ resource "ibm_pi_image" "stock_image_copy" {
 
 resource "ibm_pi_instance" "instance" {
   pi_cloud_instance_id = local.pid
-  pi_placement_group_id = local.placement_group_id
   pi_memory            = var.memory
   pi_processors        = var.processors
   pi_instance_name     = var.instance_name
@@ -58,10 +52,11 @@ resource "ibm_pi_instance" "instance" {
   pi_image_id          = length(local.private_image_id) == 0 ? ibm_pi_image.stock_image_copy[0].image_id : local.private_image_id
   pi_sys_type          = var.sys_type
   pi_storage_type      = var.storage_type
+  pi_key_pair_name     = length(data.ibm_pi_key.key) > 0 ? data.ibm_pi_key.key.id : null
+  pi_affinity_policy   = length(var.affinity_policy) > 0 ? var.affinity_policy : null
+  pi_affinity_instances = length(var.pvm_instances) > 0 ? [var.pvm_instances] : null
+  pi_placement_group_id = local.placement_group_id
   pi_license_repository_capacity = var.license_repository_capacity
-  for_each    = {for k,v in local.options: k => v if v != null}
-  key         = each.key
-  value       = each.value
   pi_network {
     network_id = data.ibm_pi_network.network_1.id
   }
